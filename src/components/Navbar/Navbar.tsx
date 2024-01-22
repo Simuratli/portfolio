5b31a13594a7899fd2155e636436d13666a34766
simuratli <simuratli@gmail.com>
2024-01-22 20:27:51 +0400

    navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,41 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Star from "../../assets/star";
+import Logo from "../../assets/images/logo.svg";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Star />
+      <Star top={-50} right={-50} />
       <div className="navbar__content">
-        <div className="navbar__logo"></div>
-        <div className="navbar__element">
-          <Link to="/about">About</Link>
+        <span className="navbar__borderDot"></span>
+        <span className="navbar__borderDot"></span>
+        <span className="navbar__borderDot"></span>
+        <span className="navbar__borderDot"></span>
+        <div className="navbar__logo">
+          <NavLink to="/">
+            <img src={Logo} alt="Star" />
+            simuratli
+          </NavLink>
+
+          <div className="hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
         </div>
-        <div className="navbar__element">
-          <Link to="/portfolio">Portfolio</Link>
-        </div>
-        <div className="navbar__element">
-          <Link to="/hire">Hire Me</Link>
+        <div className="navbar__element__container">
+          <div className="navbar__element">
+            <NavLink to="/about">About</NavLink>
+          </div>
+          <div className="navbar__element">
+            <NavLink to="/portfolio">Portfolio</NavLink>
+          </div>
+          <div className="navbar__element">
+            <NavLink to="/hire">Hire Me</NavLink>
+          </div>
         </div>
       </div>
-      <Star />
+      <Star left={-70} />
     </nav>
   );
 };
