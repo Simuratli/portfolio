b4ad9e024f81d1e4a85834856de50aaa68ea705f
Simuratli Eljan <simuratli@gmail.com>
2024-01-22 18:08:51 +0400

    initial

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/global.scss";
 import App from "./App";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+import { Navbar, Footer } from "components";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      {/*<Route path="dashboard" element={<App />} />*/}
+    </Route>,
+  ),
+);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Navbar />
+    <RouterProvider router={router} />
+    <Footer />
   </React.StrictMode>,
 );
